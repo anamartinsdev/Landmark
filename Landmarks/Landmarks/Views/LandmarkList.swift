e5254acbf44aa5ea15c0706d3bf14bf78e5f28e7
Ana Carolina Martins Pessoa <anamartinspessoa@icloud.com>
2022-02-24 18:06:43 -0300

    Generate Previews Dinamically

--- Landmarks/Landmarks/Views/LandmarkList.swift
+++ Landmarks/Landmarks/Views/LandmarkList.swift
@@ -20,10 +20,13 @@ struct LandmarkList: View {
         .navigationTitle("Landmarks")
         }
 }
+}
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
+            LandmarkList()
+            .previewDevice(PreviewDevice(rawValue: deviceName))
     }
 }
 }
